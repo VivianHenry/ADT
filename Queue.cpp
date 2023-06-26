4ab41a8288156e3d93b1bb1bc52b22ccd0876211
Vivian Henry Paul <67223688+VivianHenry@users.noreply.github.com>
2023-06-25 21:06:06 -0400

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -1,8 +1,3 @@
-// Honor Pledge:
-//
-// I pledge that I have neither given nor received any help
-// on this assignment.
-
 #include "Queue.h"
 
 //
@@ -69,18 +64,6 @@ void Queue <T>::dequeue (void)
 
     if (cur_size == 0) {throw empty_exception();}
     
-    // COMMENT This design is OK, but it is not the best design. This will be
-    // a very expensive array to use if you are dequeing a lot of elements. This
-    // is because you are copying N elements each time you dequeue 1 element.
-    // Instead, you only want to copy element when necessary. Come up with a better
-    // design that is not as expensive for the client to use.
-    // ANSWER I chose to implement the previous design (maintained in comments) in order to 
-    // maintain the logic and functionality of the size function from assignment 1. I seem 
-    // to have overlooked the performance implications of doing so. I have this changed the 
-    // logic. On dequeue I merely move the front pointer forward. Once the queue is empty, 
-    // I resize to 0. The size function in Queue.inl has been modified accordingly to 
-    // return size as end - front + 1.
-
     front++;
     if (this->front > this->end) {my_array.resize(0);}
 }
@@ -91,9 +74,6 @@ void Queue <T>::dequeue (void)
 template <typename T>
 const Queue <T> & Queue <T>::operator = (const Queue & rhs)
 {
-    // COMMENT Check for self assignment.
-    // ANSWER The check for self assignment has been implemented by initially verifying
-    // that the address of both queues are not the same.
     if (this != &rhs)
     {
         my_array = rhs.my_array;
