22e3c7695c57dfa531628b56a08f318aef6e73af
Vivian Henry Paul <67223688+VivianHenry@users.noreply.github.com>
2023-06-25 21:04:41 -0400

    Update Fixed_Array.cpp

--- Fixed_Array.cpp
+++ Fixed_Array.cpp
@@ -1,16 +1,8 @@
-// $Id: Fixed_Array.cpp 827 2011-02-07 14:20:53Z hillj $
-
-// Honor Pledge:
-//
-// I pledge that I have neither given nor received any help
-// on this assignment.
+#include "Fixed_Array.h"
 
 //
 // Fixed_Array
 //
-
-#include "Fixed_Array.h"
-
 template <typename T, size_t N>
 Fixed_Array <T, N>::Fixed_Array (void)
 : Base_Array<T>(N)
