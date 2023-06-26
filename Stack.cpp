acb0e25632678b7d7c0e42462efbbf55312d0d44
Vivian Henry Paul <67223688+VivianHenry@users.noreply.github.com>
2023-06-25 21:07:14 -0400

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,10 +1,3 @@
-// $Id: Stack.cpp 827 2011-02-07 14:20:53Z hillj $
-
-// Honor Pledge:
-//
-// I pledge that I have neither given nor received any help
-// on this assignment.
-
 #include "Stack.h"
 
 //
@@ -69,9 +62,6 @@ void Stack <T>::pop (void)
 template <typename T>
 const Stack <T> & Stack <T>::operator = (const Stack & rhs)
 {
-    // COMMENT Check for self assignment first.
-    // ANSWER The check for self assignment has been implemented by initially verifying
-    // that the address of both stacks are not the same.
     if (this != &rhs) {my_array = rhs.my_array;}
     return *this;
 }
