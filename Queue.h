fd0d43898de55451069a105fba04eb4b37b7bb9c
Vivian Henry Paul <67223688+VivianHenry@users.noreply.github.com>
2023-06-25 21:06:29 -0400

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -1,12 +1,3 @@
-//==============================================================================
-/**
- * Honor Pledge:
- *
- * I pledge that I have neither given nor received any help
- * on this assignment.
- */
-//==============================================================================
-
 #ifndef _CS507_QUEUE_H_
 #define _CS507_QUEUE_H_
 
